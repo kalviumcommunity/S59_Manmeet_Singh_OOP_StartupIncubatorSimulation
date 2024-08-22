f4500c9b8618d507be69c7f94edcd3106b5d2316
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-08-22 16:39:37 +0530

    done

--- Incubator.cpp
+++ Incubator.cpp
@@ -4,15 +4,21 @@
 Incubator::Incubator(std::string name) : name(name) {
 }
 
-Incubator& Incubator::accept_startup(const Startup& startup) {
+Incubator& Incubator::accept_startup(Startup* startup) {
     this->startups.push_back(startup);
-    std::cout << "Startup " << startup.get_name() << " has been accepted into " << this->name << " incubator." << std::endl;
+    std::cout << "Startup " << startup->get_name() << " has been accepted into " << this->name << " incubator." << std::endl;
     return *this;
 }
 
 void Incubator::provide_resources() {
     std::cout << "Providing resources to startups in the " << this->name << " incubator." << std::endl;
-    for (Startup& startup : this->startups) {
-        startup.get_funding(10000);
+    for (Startup* startup : this->startups) {
+        startup->get_funding(10000);
+    }
+}
+
+Incubator::~Incubator() {
+    for (Startup* startup : this->startups) {
+        delete startup;
     }
 }
